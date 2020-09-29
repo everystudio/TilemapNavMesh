d3b74080a0316eb58f4fc27f29248f903b2cc3ba
watanabe@tiffa <watanabe.everystudio@gmail.com>
2020-09-29 21:23:26 +0900

    プロジェクト追加

---
+++ Assets/TestDrawMovePath.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[DefaultExecutionOrder(10)]
+public class TestDrawMovePath : MonoBehaviour
+{
+    [SerializeField] LineRenderer line;
+
+    [SerializeField] Transform startPos, endPos;
+
+    private NavMeshPath path;
+
+    void Awake()
+    {
+        path = new NavMeshPath();
+    }
+
+    void Update()
+    {
+        if (startPos != null && endPos != null)
+        {
+            var result = NavMesh.CalculatePath(startPos.position, endPos.position, NavMesh.AllAreas, path);
+            enabled = line.enabled = result;
+            if (result)
+            {
+                var corners = path.corners;
+                line.positionCount = corners.Length;
+                line.SetPositions(corners);
+            }
+            else
+            {
+                //Debug.Log(enabled);
+            }
+
+        }
+    }
+}
