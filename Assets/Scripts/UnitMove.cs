0b67bdccabbfe96c1c166f0675132be5544fa837
watanabe@tiffa <watanabe.everystudio@gmail.com>
2021-04-30 13:33:14 +0900

    移動する処理を実装

---
+++ Assets/Scripts/UnitMove.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitMove : MonoBehaviour
+{
+    public GameObject m_goTarget;
+
+    [SerializeField]
+    private int m_iCurrentIndex;
+    private NavMeshPath m_moveRoutePath;
+    public float m_fSpeed = 1.0f;
+
+    public void OnClickButton()
+    {
+        m_moveRoutePath = new NavMeshPath();
+        m_iCurrentIndex = 0;
+        var result = NavMesh.CalculatePath(
+            transform.position,
+            m_goTarget.transform.position,
+            NavMesh.AllAreas, m_moveRoutePath);
+    }
+    public bool Move()
+    {
+        var tragetPosition = m_moveRoutePath.corners[m_iCurrentIndex];
+        if (Vector3.Distance(transform.position, tragetPosition) < 0.2f)
+        {
+            if(m_iCurrentIndex + 1 < m_moveRoutePath.corners.Length)
+            {
+                m_iCurrentIndex += 1;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, tragetPosition, m_fSpeed * Time.deltaTime);
+
+        return false;
+    }
+
+
+
+    void Update()
+    {
+        if(m_moveRoutePath != null)
+        {
+            if (Move())
+            {
+                m_moveRoutePath = null;
+            }
+        }
+    }
+}
